c7826b13f928a9daaa34dcb56c207722bde43660
Devesh Venkatesh <devesh.venkatesh@gmail.com>
2021-01-10 15:57:32 -0800

    Added some more css

--- myFunc.js
+++ myFunc.js
@@ -1,12 +1,93 @@
-function myFunc(liquor) {
-    document.getElementById("title").style.backgroundColor = "green";
-    var para = document.createElement("p");
-    var node = document.createTextNode(liquor);
-    para.appendChild(node);
-    var element = document.getElementById("form");
-    element.appendChild(para);
-    
-}
+// cocktails array with cocktail objects
+var cocktails = [
+    {
+        name: "Manhattan",
+        ingredients: ["whiskey", "sweet vermouth", "angostura"],
+        prep:  `Two ounces of whiskey<br>
+        One ounce of sweet vermouth<br>
+        Two dashes of angostura`
+    },
+    {
+        name: "Gimlet",
+        ingredients: ["gin", "lime", "simple syrup"],
+        prep: `Two ounces of gin<br>
+        One ounce of simple syrup<br>
+        Juice of half a lime` 
+    },
+    {
+        name: "Tequila Sunrise",
+        ingredients: ["tequila", "orange juice", "grenadine"],
+        prep: `Two ounces of tequila<br>
+        Four ounces of orange juice<br>
+        One dash of grenadine` 
+    },
+    {
+        name: "Whiskey Old-Fashioned",
+        ingredients: ["whiskey", "simple syrup", "angostura"],
+        prep: `Two ounces of whiskey<br>
+        Half an ounce of simple syrup<br>
+        Two dashes of angostura` 
+    },
+    {
+        name: "Martini",
+        ingredients: ["gin", "dry vermouth", "orange bitters"],
+        prep: `Two ounces of gin<br>
+        One ounce of dry vermouth<br>
+        One dash of orange bitters` 
+    },
+    {
+        name: "Cosmopolitan",
+        ingredients: ["vodka", "orange liqueur", "lime", "cranberry juice"],
+        prep: `Two ounces of gin<br>
+        One ounce of orange liqueur<br>
+        Juice of half a lime<br>
+        One dash of cranberry juice` 
+    },
+    {
+        name: "Margarita",
+        ingredients: ["tequila", "orange liqueur", "lime", "simple syrup"],
+        prep: `Two ounces of tequila<br>
+        Half an ounce of orange liqueur<br>
+        Juice of half a lime<br>
+        Half an ounce of simple syrup` 
+    },
+    {
+        name: "Tom Collins",
+        ingredients: ["gin", "lemon", "soda", "simple syrup"],
+        prep: `Two ounces of gin<br>
+        Half an ounce of simple syrup<br>
+        Juice of half a lemon<br>
+        Top with soda` 
+    },
+    {
+        name: "Daiquiri",
+        ingredients: ["rum", "lime", "simple syrup"],
+        prep: `Two ounces of rum<br>
+        One ounce of simple syrup<br>
+        Juice of half a lime` 
+    },
+    {
+        name: "Bee's Knees",
+        ingredients: ["gin", "lemon", "honey syrup"],
+        prep: `Two ounces of gin<br>
+        Half an ounce of honey syrup<br>
+        Juice of half a lemon` 
+    },
+    {
+        name: "Negroni",
+        ingredients: ["gin", "campari", "sweet vermouth"],
+        prep: `One ounce of gin<br>
+        One ounce of campari<br>
+        One ounce of sweet vermouth` 
+    },
+    {
+        name: "Sidecar",
+        ingredients: ["brandy", "orange liqueur", "lemon"],
+        prep: `Two ounces of brandy<br>
+        One ounce of orange liqueur<br>
+        Juice of half a lemon` 
+    }     
+];
 
 // function is called when user presses submit button
 function allInputs() {
@@ -24,32 +105,44 @@ function allInputs() {
     lookUp(selected);
 }
 
-var cocktails = [
-    {
-        name: "manhattan",
-        ingredients: ["whiskey", "sweet vermouth", "angostura"],
-        prep: "Two ounces of whiskey\nTOne ounce sweet vermouth\nTwo dashes angostura"
-    },    
-];
-
+// checks which cocktails can be made,
+// array of user selected ingredients is passed in
 function lookUp(selectedArr) {
     let results = [];
     for (var i = 0; i < cocktails.length; i++) {
-        console.log(cocktails[i].ingredients);
-        console.log(selectedArr);
-        console.log(cocktails[i]);
-        //let allFound = cocktails[i].ingredients.every( ai =>  selectedArr.includes(ai) );
         var thing = cocktails[i].ingredients.every(r => selectedArr.includes(r));
-        console.log(thing);
         if (thing) {
             results.push(cocktails[i].name);
             putIt(cocktails[i]); 
         }
     }
-    console.log(results);
 }
 
+// prints out cocktail recipes onto webpage
 function putIt(result) {
-    console.log("Inside put it" + result);
+    // store object properties in local variables
+    var cocktailName = result.name;
+    var ingred = result.ingredients;
+    var prepare = result.prep;
 
+    // heading element for cocktail name
+    var hedd = document.createElement("H3");
+    hedd.innerHTML = cocktailName;
+    document.getElementById("recipes").appendChild(hedd);
+
+    // ul element for cocktail ingredients
+    var list = document.createElement("UL");
+    for (var i = 0; i < ingred.length; i++) {
+        var item = document.createElement("LI");
+
+        item.appendChild(document.createTextNode(ingred[i]));
+
+        list.appendChild(item);
+    }
+    document.getElementById("recipes").appendChild(list);
+
+    // p element for instructions
+    var instruct = document.createElement("P");
+    instruct.innerHTML = prepare;
+    document.getElementById("recipes").appendChild(instruct);
 }
