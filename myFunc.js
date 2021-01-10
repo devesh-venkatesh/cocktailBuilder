5e5dc2d29d6b6edf219b29f568db1dd2c94396ca
Devesh Venkatesh <devesh.venkatesh@gmail.com>
2021-01-10 12:48:12 -0800

    Added lookUp function

--- myFunc.js
+++ myFunc.js
@@ -12,12 +12,44 @@ function myFunc(liquor) {
 function allInputs() {
     // take all of the ingredients and put them in a NodeList Object
     var total = document.querySelectorAll(".ing");
+    // create array variable for results
     var selected = [];
+    // sift through each ingredient and push the checked ones
     for(var i = 0; i < total.length; i++) {
         if(total[i].checked) {
             selected.push(total[i].value);
         }
     }
-    
-    console.log(selected);
+    // call function and pass ingredients array
+    lookUp(selected);
+}
+
+var cocktails = [
+    {
+        name: "manhattan",
+        ingredients: ["whiskey", "sweet vermouth", "angostura"],
+        prep: "Two ounces of whiskey\nTOne ounce sweet vermouth\nTwo dashes angostura"
+    },    
+];
+
+function lookUp(selectedArr) {
+    let results = [];
+    for (var i = 0; i < cocktails.length; i++) {
+        console.log(cocktails[i].ingredients);
+        console.log(selectedArr);
+        console.log(cocktails[i]);
+        //let allFound = cocktails[i].ingredients.every( ai =>  selectedArr.includes(ai) );
+        var thing = cocktails[i].ingredients.every(r => selectedArr.includes(r));
+        console.log(thing);
+        if (thing) {
+            results.push(cocktails[i].name);
+            putIt(cocktails[i]); 
+        }
+    }
+    console.log(results);
+}
+
+function putIt(result) {
+    console.log("Inside put it" + result);
+
 }
